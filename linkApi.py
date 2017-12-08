5b36c63149ef07a254c0e8f2741778e16807074f
ffactory-ofcl <filippoorru@hotmail.com>
2017-12-08 14:52:27 +0100

    Me is not a smart man...

--- linkApi.py
+++ linkApi.py
@@ -236,13 +236,13 @@ def getTopLinkRatings():
     errorCode = None
     links = []
     if check.DbIsNotEmpty('ratings'):
-        result = databaseConnection.executeMDb('ratings', 'find', {}, 'all',
-                                               ['allLinkRating', -1])
-        print('res: ' + str(result))
+        result = databaseConnection.executeMDb(
+            'ratings', 'find', {}, 'all', ['allLinkRating', -1]['dbReturn'])
+        #print('res: ' + str(result))
         #result = databaseConnection.executeSql(
         #    "SELECT * FROM `ratings` ORDER BY allLinkRating DESC", '', 3)
         for row in result:
-            links.append(row)
+            links.append(row['link'])
     errorCode = 1
     return {'errorCode': errorCode, 'links': links}
 
